9ca3afda8787639be744bcf9d5bad9d11627ba2d
Allison Snipes <ajsnipes@usfca.edu>
2019-04-07 01:01:26 -0700

    working on errors

--- assests/js/app.js
+++ assests/js/app.js
@@ -11,9 +11,7 @@ var timer = 30;//set the number counter to 30
 var timerOn = false; //default to have the time on off
 var introWrapper = document.getElementById("intro"); //introduction wrapepr
 var displayQs = document.getElementById("questionsQ"); //quiz wrapper
-const choiceLetter = $(`input[name='quizchoices']:checked`).val();
-const wrongAnswerText = `I'm sorry, that was the incorrect answer. The correct answer is: ${questions[currentQuestion].correctAnswer}`;
-const rightAnswerText = "Good job! Right answer.";
+const percentage = ((score/10) * 100);
 var questions = [
   {
     question: "Which country ranks number one in press freedom?",
@@ -128,14 +126,11 @@ var questions = [
 
 
 //initializing buttons for hide functions for start button/ next button
-$("#startQuiz").on("click", () => start());
+$(".startButton").on("click", () => start());
 
-//initializing quiz progress
-$("nextButton").on("click", () => nextQuestion());
-
-//functions for initialized buttons etc
-var start = () => {
-  $("#questionsQ").show();
+//startButton function for initialized buttons etc
+let start = () => {
+  $(".quiz").show();
   if (introWrapper.style.display == 'none') {
     introWrapper.style.display = 'block';
     finishedSection.style.display = 'none';
@@ -146,7 +141,9 @@ var start = () => {
   }
 };
 
-var timer
+//initializing quiz progress
+$(".nextButton").on("click", () => nextQuestion());
+
 // dislpays quiz questions for next question clicked
 var nextQuestion = () => {
   console.log("display questions");
@@ -155,34 +152,54 @@ var nextQuestion = () => {
     introWrapper.style.display = 'none';
     questionsQ.style.display = 'none';
 
-    if (choiceLetter === questions[currentQuestion].correctAnswer) {//progress
-       $(".rightFeebackPart").text(rightAnswerText).show();
-       $(".wrongFeebackPart").hide();
-       score++;
-      } else{
-        $(".wrongFeebackPart").show().text(wrongAnswerText);//show right ans
-          $(".rightFeebackPart").hide();
+    $(".nextButton").on("click", function(event) {
+      event.preventDefault();
+      const choiceLetter = $(`input[name='quizchoices']:checked`).val();
+      const wrongAnswerText = `I'm sorry, that was the incorrect answer. The correct answer is: ${questions[currentQuestion].correctAnswer}`;
+      const rightAnswerText = "Good job! Right answer.";
+
+      if ((currentQuestion + 1) === questions.length) { //if the user gets to the last question stop the quiz
+          $(".finishedQuiz").show();
+          $(".wrapper").hide();
+          $(".quiz").hide();
+
+          if (choiceLetter === questions[currentQuestion].correctAnswer) {//code to generate next question if the user provides right answer
+              $(".rightFeebackPart").text(rightAnswerText).show();
+              $(".wrongFeebackPart").hide();
+              score++;
+          } else{
+              $(".wrongFeebackPart").show().text(wrongAnswerText);//code to let the user know they did not get the correct answer, and provide the correct answer
+              $(".rightFeebackPart").hide();
+          }
+          calculatePercentage("You're final score is: ");//informs the user of their final score
+          resetQuiz();
+          exitQuiz();
+      } else { //if else statement for the user to continue the quiz until the reach the last question
+          if (choiceLetter === questions[currentQuestion].correctAnswer) {
+              $(".rightFeebackPart").text(rightAnswerText).show(); //generate next question if the user gets the question right
+              $(".wrongFeebackPart").hide();
+              score++;//increase the users' score if the get the question right
+              currentQuestion++;//show the next question if the user gets the previous question correct
+          } else if ($(`input[name='quizchoices']:checked`).length <= 0) {//prevents the user from skipping the question without providing an answer choice
+              alert ("Please make an answer selection.");//displays an error message to let the user know they have to select an answer before moving on
+          } else {
+              $(".wrongFeebackPart").show().text(wrongAnswerText);//informs the user that they selected the wrong answer and shows the correct answer instead
+              $(".rightFeebackPart").hide();
+              currentQuestion++;//moves on to the next question
+          }
+          calculatePercentage("You're current score is: ");//informs the user of their current score
+          $(".quizLocation").html(`You're on question: ${currentQuestion}`);//informs the user of their location in the quiz
+          questionDisplay();
+          generateQuestion();
       }
-      resetQuiz();
-      exitQuiz();
-   } else { //if else statement to continue the quiz until last question
-    if (choiceLetter === questions[currentQuestion].correctAnswer) {
-      $(".rightFeebackPart").text(rightAnswerText).show(); //generate next question if the user gets the question right
-      $(".wrongFeebackPart").hide();
-      score++;//increase the users' score if the get the question right
-      currentQuestion++;//show the next question if question correct
-    } else if ($(`input[name='quizchoices']:checked`).length <= 0) {//prevents users from skipping ques
-      alert ("Please make an answer selection.");//displays an error message
-    } else {
-      $(".wrongFeebackPart").show().text(wrongAnswerText);//informs the user that they selected the wrong answer and shows the correct answer instead
-      $(".rightFeebackPart").hide();
-      currentQuestion++;//moves on to the next question
-    }
-  }
+  });
 }
 
+// var timer
+
 //function to generate question
-$(".quizQuestions").html(`
+function generateQuestion() {
+  $(".quizQuestions").html(`
     <legend>
       ${questions[currentQuestion].question}
     </legend>
@@ -193,20 +210,19 @@ $(".quizQuestions").html(`
     </div>
 
     <div>
-        <input id="${questions[currentQuestion].answers.b}" type="radio" name="quizchoices" value="${questions[currentQuestion].answers.b}">
-        <label for="${questions[currentQuestion].answers.b}"> ${questions[currentQuestion].answers.b}</label>
+      <input id="${questions[currentQuestion].answers.b}" type="radio" name="quizchoices" value="${questions[currentQuestion].answers.b}">
+      <label for="${questions[currentQuestion].answers.b}"> ${questions[currentQuestion].answers.b}</label>
     </div>
 
     <div>
-        <input id="${questions[currentQuestion].answers.c}" type="radio" name="quizchoices" value="${questions[currentQuestion].answers.c}">
-        <label for="${questions[currentQuestion].answers.c}"> ${questions[currentQuestion].answers.c}</label>
+      <input id="${questions[currentQuestion].answers.c}" type="radio" name="quizchoices" value="${questions[currentQuestion].answers.c}">
+      <label for="${questions[currentQuestion].answers.c}"> ${questions[currentQuestion].answers.c}</label>
     </div>
 
     <div>
-        <input id="${questions[currentQuestion].answers.d}" type="radio" name="quizchoices" value="${questions[currentQuestion].answers.d}">
-        <label for="${questions[currentQuestion].answers.d}"> ${questions[currentQuestion].answers.d}</label>
+      <input id="${questions[currentQuestion].answers.d}" type="radio" name="quizchoices" value="${questions[currentQuestion].answers.d}">
+      <label for="${questions[currentQuestion].answers.d}"> ${questions[currentQuestion].answers.d}</label>
     </div>
     <button class="nextButton" id="nextButton" type="submit">Next Question </button>
   `);
-
-//code here for if/else wrong answer
+}
